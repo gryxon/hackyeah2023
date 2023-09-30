6adf7e6274a0e6c6f2a140b8b7a842066708dd2c
GrzegorzKania-SmartNIC <grzegorz.kania@codilime.com>
2023-10-01 00:37:19 +0200

    Status success.

--- game.js
+++ game.js
@@ -138,9 +138,14 @@ function swapBlank(folderName, newBlankPosition) {
      blankPosition = newBlankPosition;
      renderBoard();
      if (isCorrect(folderName)) {
-       setTimeout(function(){ alert("Dobrze!") }, 300);
+       setTimeout(function(){ addSuccessDiv() }, 300);
      }
 }
+
+function addSuccessDiv() {
+    let svg_anim = '<div class="success-animation"><svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none" /><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" /></svg></div>'
+    document.getElementById("anim_div").innerHTML += svg_anim
+  }
 
 function setDifficulty(boardSize) {
     Cookies.set("boardSize", boardSize);
